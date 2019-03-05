604247d61f132dee5b9fa9801ad56ab8e5baa614
Ryan Mui <reachryanmui@gmail.com>
2019-03-05 04:01:56 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,12 +1,16 @@
 class Patient
-  attr_accessor :name
+  attr_accessor :name, :doctors
   @@all = []
 
   def initialize(name)
     @name = name
+    @doctors = []
     @@all << self
   end
 
+  def new_appointment(doctor, date)
+    appointment = Appointment.new(self, date, doctor)
+    appointments
   def self.all
     @@all
   end
